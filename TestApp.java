c7d2f9127f0bb7494bbd5311e977484d64fea9e0
Nikolay-Varbanov <nikolayvurbanov86@gmail.com>
2023-10-26 11:30:08 +0200

    - Testing Windows icacls premmision change

--- TestApp.java
+++ TestApp.java
@@ -1,14 +1,6 @@
 class TestApp {
 	public static void main(String[] args) {
 		System.out.println("Hey I'm Working here");
-	
-		/*Executable myEnnityA = new EntityA();
-		
-		myEnnityA.OnExecute();
-		
-		Executable myEnnityB = new EntityB();
-		
-		myEnnityB.OnExecute();*/
 		
 		Executable myEntityList = new EntityList();
 		
